beae3783a7d43409353b89d58c19c58e56b336cf
ElisaBerlin <grados.elisa@gmail.com>
2024-04-28 09:40:39 +0200

    cambio1

--- ent4y5/src/ent4y5/Main.java
+++ ent4y5/src/ent4y5/Main.java
@@ -2,7 +2,7 @@ package ent4y5;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hallo Welt!");
 		Suma s = new Suma(2,3);
 		System.out.println(s.sumar());
 	}
